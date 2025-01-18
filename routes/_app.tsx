e2e57e99f786c48acb754825a79d18f5de378751
sinyo-matu <sinyo_matu@icloud.com>
2025-01-18 23:54:13 +0900

    add seo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,13 +6,13 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>ramen-hunt</title>
+        <title>Ramen Hunter</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
